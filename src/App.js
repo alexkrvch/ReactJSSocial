2c7782c92455204661592c158dd69aa195c3caed
Alex <heikun@yandex.ru>
2023-08-18 00:02:14 +0300

    Lesson-7

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ const App = () => {
     return (
         <div className="App">
             <div className="App-content">
-                <header className="App-header">
-                    <img srcSet={logo} alt="" />
-                </header>
-                <nav className="App-nav">
-
-                </nav>
-                <div className="App-pagecontent">
-
-                </div>
-                <footer className="App-footer">
-
-                </footer>
+                <Header />
+                <Nav />
+                <Content />
+                <Footer />
             </div>
         </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <header className="App-header">
+            <img srcSet={logo} alt="" />
+        </header>
+    );
+}
+
+const Nav = () => {
+    return (
+        <nav className="App-nav">
+            Text nav
+        </nav>
+    );
+}
+
+const Content = () => {
+    return (
+        <div className="App-pagecontent">
+            Content
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer className="App-footer">
+            Footer
+        </footer>
     );
 }
 
