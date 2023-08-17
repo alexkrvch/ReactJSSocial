b2b636a662212b27185b038c5774243bbfa769cf
Alex <heikun@yandex.ru>
2023-08-17 22:51:00 +0300

    Lesson-6 add logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import logo from './logo.svg';
 
 const App = () => {
     return (
         <div className="App">
             <div className="App-content">
-                <h1>React JS Pet Start</h1>
+                <header className="App-header">
+                    <img srcSet={logo} alt="" />
+                </header>
+                <nav className="App-nav">
+
+                </nav>
+                <div className="App-pagecontent">
+
+                </div>
+                <footer className="App-footer">
+
+                </footer>
             </div>
         </div>
     );
