55f68b1fadba99a93cded5f169e78a9e21596343
Alex <heikun@yandex.ru>
2023-10-03 20:08:55 +0300

    Lesson-20

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,16 @@
 import s from './Nav.module.css'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
     return (
         <nav className={s.sidebar_nav}>
             <div className={s.sidebar}>
                 <ul className={s.sidebar_menu}>
-                    <li className={`${s.menu_item} ${s.menu_active}`}><Link to="/profile">Profile</Link></li>
-                    <li className={s.menu_item}><Link to="/messages">Messages</Link></li>
-                    <li className={s.menu_item}><Link to="/news">News</Link></li>
-                    <li className={s.menu_item}><Link to="/music">Music</Link></li>
-                    <li className={s.menu_item}><Link to="/settings">Settings</Link></li>
+                    <li className={`${s.menu_item}`}><NavLink to="/profile" className={({ isActive }) => isActive ? s.menu_active : ""}>Profile</NavLink></li>
+                    <li className={s.menu_item}><NavLink to="/messages" className={({ isActive }) => isActive ? s.menu_active : ""}>Messages</NavLink></li>
+                    <li className={s.menu_item}><NavLink to="/news" className={({ isActive }) => isActive ? s.menu_active : ""}>News</NavLink></li>
+                    <li className={s.menu_item}><NavLink to="/music" className={({ isActive }) => isActive ? s.menu_active : ""}>Music</NavLink></li>
+                    <li className={s.menu_item}><NavLink to="/settings" className={({ isActive }) => isActive ? s.menu_active : ""}>Settings</NavLink></li>
                 </ul>
             </div>
         </nav>
