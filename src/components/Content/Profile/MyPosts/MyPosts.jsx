104678ad298ee3bf57dbbb66c527e156b0098bfb
Alex <heikun@yandex.ru>
2023-10-03 23:12:50 +0300

    Lesson-24

--- src/components/Content/Profile/MyPosts/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,25 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let PostData = [
+        {id: 1, header: 'First post', text: 'Nullam euismod,1 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien.', countLikes: 3, date: '09 September 2023'},
+        {id: 1, header: 'Second post', text: 'Nullam euismod,2 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien.', countLikes: 5, date: '02 September 2023'},
+        {id: 1, header: 'Next post', text: 'Nullam euismod,3 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien.', countLikes: 12, date: '28 August 2023'},
+        {id: 1, header: 'New post', text: 'Nullam euismod,4 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien.', countLikes: 2, date: '21 August 2023'},
+    ]
+
     return (
         <div className={s.my_posts}>
-            <Post header="First post" text="Nullam euismod,1 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien." countLikes="3" date="09 September 2023" />
-            <Post header="Second post" text="Nullam euismod,2 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien." countLikes="5" date="02 September 2023" />
-            <Post header="Next post" text="Nullam euismod,3 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien." countLikes="12" date="28 August 2023" />
-            <Post header="New post" text="Nullam euismod,4 diam vel tincidunt bibendum, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien, velit sapien bibendum sapien, vel bibendum sapien sapien vel sapien." countLikes="2" date="21 August 2023" />
+
+            <form className={s.form}>
+                <textarea placeholder='New post' ></textarea>
+                <input type='button' value='Send' />
+            </form>
+
+            <hr />
+
+            {PostData.map(p => <Post header={p.header} text={p.text} countLikes={p.countLikes} date={p.date} />)}
         </div>
     );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
             <div className={s.sidebar}>
                 <ul className={s.sidebar_menu}>
                     <li className={`${s.menu_item}`}><NavLink to="/profile" className={({ isActive }) => isActive ? s.menu_active : ""}>Profile</NavLink></li>
-                    <li className={s.menu_item}><NavLink to="/messages" exact className={({ isActive }) => isActive ? s.menu_active : ""}>Messages</NavLink></li>
+                    <li className={s.menu_item}><NavLink to="/messages" className={({ isActive }) => isActive ? s.menu_active : ""}>Messages</NavLink></li>
                     <li className={s.menu_item}><NavLink to="/news" className={({ isActive }) => isActive ? s.menu_active : ""}>News</NavLink></li>
                     <li className={s.menu_item}><NavLink to="/music" className={({ isActive }) => isActive ? s.menu_active : ""}>Music</NavLink></li>
                     <li className={s.menu_item}><NavLink to="/settings" className={({ isActive }) => isActive ? s.menu_active : ""}>Settings</NavLink></li>
