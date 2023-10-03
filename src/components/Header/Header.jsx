c0bac708d0766b83e2d8158b41ef377478df1135
Alex <heikun@yandex.ru>
2023-10-03 17:03:09 +0300

    Lesson-15

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,8 @@ import s from './Header.module.css'
 const Header = () => {
     return (
         <header className={s.header_page}>
-            <nav>
-                <img alt="" className={s.logo} src={logo} />
-                WebArcticFox React
-            </nav>
+            <img alt="" className={s.logo} src={logo} />
+            WebArcticFox React
         </header>
     );
 }
