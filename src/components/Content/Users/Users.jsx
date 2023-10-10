b795992de03cc95d9fee4d511ed54c6130769dae
Alex <heikun@yandex.ru>
2023-10-11 00:15:09 +0300

    Lesson-53 try class component

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -6,18 +6,19 @@ import React from "react";
 
 class Users extends React.Component {
 
-    getUsers = () => {
+    constructor(props) {
+        super(props);
+
         axios.get('https://social-network.samuraijs.com/api/1.0/users?page=650').then( data => {
             this.props.setUsers(data.data.items)
         })
     }
 
-
     render = () => {
         let users = this.props.users.map(u => <User key={u.id} id={u.id} name={u.name} photos={u.photos} status={u.status} followed={u.followed} follow={this.props.follow} unFollow={this.props.unFollow} />)
         return(
         <div className={s.users}>
-            {users.length ? users : <button onClick={this.getUsers}>Get users</button>}
+            {users.length ? users : ''}
         </div>
         )
     }
