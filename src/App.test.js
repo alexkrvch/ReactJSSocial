de1828a7ed29b3e4f68b344fa723a05f202e64fd
Alex <heikun@yandex.ru>
2023-10-26 00:07:50 +0300

    Lesson-92

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
-
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import store from "./redux/redux-store";
+import {Provider} from "react-redux";
 
 test('renders learn react link', () => {
-  expect(1).toBe(1);
+  const {container} = render(<Provider store={store}><App /></Provider>);
+  const box = container.getElementsByClassName('preloaderContainer');
+  expect(box.length).toBe(1);
 });
