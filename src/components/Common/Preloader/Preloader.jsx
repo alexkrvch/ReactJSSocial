e4b270d731caaa149c82b054462f17d883f8c330
Alex <heikun@yandex.ru>
2023-10-27 16:44:56 +0300

    Lesson-94

--- src/components/Common/Preloader/Preloader.jsx
+++ src/components/Common/Preloader/Preloader.jsx
@@ -1,11 +1,25 @@
-import preloader from "../../../assets/img/oval.svg";
 import s from "./Preloader.module.css";
 import React from "react";
 
 const Preloader = () => {
     return (
         <div className={s.preloaderContainer}>
-            <img src={preloader} alt='' className={s.preloader} />
+            <svg width="300" height="300" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+                <g fill="none" fillRule="evenodd">
+                    <g transform="translate(1 1)" strokeWidth="1">
+                        <circle strokeOpacity=".05" cx="18" cy="18" r="18"/>
+                        <path d="M36 18c0-9.94-8.06-18-18-18">
+                            <animateTransform
+                                attributeName="transform"
+                                type="rotate"
+                                from="0 18 18"
+                                to="360 18 18"
+                                dur="1s"
+                                repeatCount="indefinite"/>
+                        </path>
+                    </g>
+                </g>
+            </svg>
         </div>
     )
 }
