15c71a79638cbf6d581783950d297f79df1cda37
Alex <heikun@yandex.ru>
2023-11-07 22:39:38 +0300

    Lesson-2.0-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import './App.css';
-import NavContainer from "./components/Nav/NavContainer.tsx";
+import NavContainer from "./components/Nav/NavContainer";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 import {BrowserRouter} from "react-router-dom";
-import HeaderContainer from "./components/Header/HeaderContainer.tsx";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import {Component} from "react";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import React from 'react';
+import {AppStateType} from "@/redux/redux-store.ts";
 
-class App extends Component<{}, { props:any  }> {
-    catchAllUnhandledErrors = (error:any):void => {
+type MapStatePropsType = {
+    initialized:boolean
+}
+
+type MapDispatchPropsType = {
+    initializeApp: () => void
+}
+
+type PropsType = MapStatePropsType & MapDispatchPropsType
+
+class App extends Component<PropsType> {
+    catchAllUnhandledErrors = (e: PromiseRejectionEvent) => {
         alert('some error');
     }
 
@@ -47,10 +58,10 @@ class App extends Component<{}, { props:any  }> {
 }
 
 
-const mapStateToProps = (state:any):{initialized:any} => ({
+const mapStateToProps = (state:AppStateType) => ({
     initialized: state.App.initialized
 })
 
-export default compose(
+export default compose<React.ComponentType>(
     connect(mapStateToProps, {initializeApp})
 )(App);
