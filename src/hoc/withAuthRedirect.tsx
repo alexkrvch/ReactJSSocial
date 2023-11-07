8b556d907eceaaa87d6f7b56db5888409f8849f2
Alex <heikun@yandex.ru>
2023-11-07 23:04:10 +0300

    Lesson-2.0-11

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import {Navigate} from "react-router-dom";
 import {connect} from "react-redux";
+import {AppStateType} from "@/redux/redux-store.ts";
 
-let mapStateToPropsRed = (state) => ({
+let mapStateToPropsRed = (state:AppStateType):MapPropsType => ({
     isAuth: state.Auth.isAuth
 })
 
-export const withAuthRedirect = (Component) => {
-    class RedirectComponent extends React.Component {
-        render() {
-            if(!this.props.isAuth) { return <Navigate to={"/login"} />}
-            return <Component {...this.props} />
-        }
+type MapPropsType = {
+    isAuth: boolean
+}
+
+export function withAuthRedirect<WCP>(Component:React.ComponentType<WCP>) {
+    function RedirectComponent(props:MapPropsType) {
+        let {isAuth, ...restProps} = props;
+        if(!props.isAuth) { return <Navigate to={"/login"} />}
+        return <Component {...restProps as unknown as WCP} />
     }
 
-    return connect(mapStateToPropsRed, {})(RedirectComponent)
+    return connect<mapStateToPropsRed, {}, WCP, AppStateType>(mapStateToPropsRed, {})(RedirectComponent);
 }
